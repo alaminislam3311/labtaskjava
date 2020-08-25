911a5ee327d9bb5c1f11b4c9d232ec2f02de4b63
alaminislam3311 <alamin.sagor3311@gmail.com>
2020-08-25 01:41:24 -0700

    Javalabtask

---
+++ src/arithme/arithme.java
@@ -0,0 +1,11 @@
+package arithme;
+  import java.math.*;
+
+public class arithme {
+	    class Adder extends arithme {
+	        public int add(int a, int b){
+	            return a+b;
+	        }
+	    }
+
+}
